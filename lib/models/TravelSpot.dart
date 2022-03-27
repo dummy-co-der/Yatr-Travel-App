bfb18f6367be1c6fc02a13b2739db1c1ba3c8715
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-03-27 10:31:31 +0530

    Flutter

---
+++ lib/models/TravelSpot.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:travel/models/User.dart';
+
+class TravelSpot {
+  final String name, image;
+  final DateTime date;
+  final List<User> users;
+
+  TravelSpot({
+    @required this.users,
+    @required this.name,
+    @required this.image,
+    @required this.date,
+  });
+}
+
+List<TravelSpot> travelSpots = [
+  TravelSpot(
+    users: users..shuffle(),
+    name: "Kedarnath",
+    image: "assets/images/kedarnath-yatra.jpg",
+    date: DateTime(2020, 10, 15),
+  ),
+  TravelSpot(
+    users: users..shuffle(),
+    name: "Badrinath",
+    image: "assets/images/badrinath-yatra.jpg",
+    date: DateTime(2020, 3, 10),
+  ),
+  TravelSpot(
+    users: users..shuffle(),
+    name: "Gangotri",
+    image: "assets/images/gangotri-yatra.jpg",
+    date: DateTime(2020, 10, 15),
+  ),
+  TravelSpot(
+    users: users..shuffle(),
+    name: "Yamunotri",
+    image: "assets/images/yamunotri-yatra.jpg",
+    date: DateTime(2020, 3, 10),
+  ),
+];
+
+List<User> users = [user1, user2, user3];
