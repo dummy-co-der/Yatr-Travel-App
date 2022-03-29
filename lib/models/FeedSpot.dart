c902de47ca16b03b307044cf00da8c629f3998a8
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-03-30 00:10:25 +0530

    updates

--- lib/models/FeedSpot.dart
+++ lib/models/FeedSpot.dart
@@ -3,14 +3,15 @@ import 'package:travel/screens/events/components/feed_screen.dart';
 
 class FeedSpot {
   final String name, image;
-  final DateTime date;
+  final String text;
   // final List<User> users;
 
   FeedSpot({
     // @required this.users,
     @required this.name,
     @required this.image,
-    @required this.date,
+    @required this.text,
+    // @required this.date,
   });
 }
 
@@ -19,28 +20,32 @@ List<FeedSpot> feedSpots = [
 // users: users..shuffle(),
     name: "John",
     image: "assets/images/John.png",
-    date: DateTime(2020, 10, 15),
+    text: "Kedarnath is the best Place I visited. It was a great experience. User Review- ⭐⭐⭐⭐⭐",
+    // date: DateTime(2020, 10, 15),
   ),
 
   FeedSpot(
 // users: users..shuffle(),
     name: "James",
     image: "assets/images/james.png",
-    date: DateTime(2020, 3, 10),
+    text: "Badrinath is the best place I visited in my life User Review- ⭐⭐⭐⭐",
+    // date: DateTime(2020, 3, 10),
   ),
 
   FeedSpot(
 // users: users..shuffle(),
     name: "Rosy",
     image: "assets/images/rosy.png",
-    date: DateTime(2020, 3, 10),
+    text: "Doing river rafting in Rishikesh was tremendous User Review- ⭐⭐⭐⭐",
+    // date: DateTime(2020, 3, 10),
   ),
 
   FeedSpot(
 // users: users..shuffle(),
     name: "Christine",
     image: "assets/images/marry.png",
-    date: DateTime(2020, 3, 10),
+    text: "Paragliding was an awesome experience User Review- ⭐⭐⭐⭐⭐",
+    // date: DateTime(2020, 3, 10),
   ),
 ];
 
